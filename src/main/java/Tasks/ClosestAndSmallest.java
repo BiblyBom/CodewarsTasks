355cb3c3281667467419bdbc3bf0c35b2ddc44b4
Maxim Boyev <maximboev90@gmail.com>
2022-07-18 20:58:13 +0300

    Added ClosestAndSmallest.java

---
+++ src/main/java/Tasks/ClosestAndSmallest.java
@@ -0,0 +1,10 @@
+package Tasks;
+
+public class ClosestAndSmallest {
+    public static void main(String[] args) {
+    }
+    public static int[][] closest(String strng) {
+    return null;
+    }
+
+}
