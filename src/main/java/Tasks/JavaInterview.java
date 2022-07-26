539634d01c3db795caf59b13f5a2b80ed2455582
Maxim Boyev <maximboev90@gmail.com>
2022-07-26 18:12:35 +0300

    Added JavaInterview.java

---
+++ src/main/java/Tasks/JavaInterview.java
@@ -0,0 +1,19 @@
+package Tasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class JavaInterview {
+    public static void main(String[] args) {
+
+    }
+
+    public static String mostFrequency (String str) {
+        String[] numbers = str.replaceAll("\\[","")
+                              .replaceAll("]", "")
+                              .replaceAll(",", "")
+                              .split(" ");
+
+        return "";
+    }
+}
