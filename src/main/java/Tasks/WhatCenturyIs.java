fec67b9ce89f92ed87c8b65c0a781d5c18d65ac2
Maxim Boyev <maximboev90@gmail.com>
2022-07-27 23:00:13 +0300

    Исправлена ошибка в WhatCenturyIs.java с разными окончаниями веков

--- src/main/java/Tasks/WhatCenturyIs.java
+++ src/main/java/Tasks/WhatCenturyIs.java
@@ -2,17 +2,21 @@ package Tasks;
 
 public class WhatCenturyIs {
     public static void main(String[] args) {
-        System.out.println(whatCentury(1900));
+        System.out.println(whatCentury(2890));
     }
 
     public static String whatCentury(int year) {
         int century;
-        if (year % 10 == 0) {
+        if (year % 100 == 0) {
             century = year / 100;
         }
         else {
             century = (int) (year / 100.0) + 1;
         }
-        return century +"th";
+        if (century == 11 || century == 12 || century == 13) {
+            return century +"th";
+        }
+        return (century % 10 == 1) ? century +"st" : (century % 10 == 2) ? century +"nd" :
+                (century % 10 == 3) ? century +"rd" : century +"th";
     }
 }
