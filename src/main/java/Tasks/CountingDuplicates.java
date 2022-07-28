65a870d3339455a5e69ffac31b5ed8c3b3109425
Maxim Boyev <maximboev90@gmail.com>
2022-07-28 21:57:07 +0300

    Исправлена ошибка в CountingDuplicates.java с последним символом

--- src/main/java/Tasks/CountingDuplicates.java
+++ src/main/java/Tasks/CountingDuplicates.java
@@ -11,8 +11,8 @@ public class CountingDuplicates {
     public static int duplicateCount(String text) {
         HashSet<Character> duplicates = new HashSet<>();
         char[] characters = text.toLowerCase().toCharArray();
-        for (int i = 0; i < characters.length; i++) {
-            for (int j = i + 1; j < characters.length - 1; j++) {
+        for (int i = 0; i < characters.length - 1; i++) {
+            for (int j = i + 1; j < characters.length; j++) {
                 if (characters[i] == characters[j]) {
                     duplicates.add(characters[i]);
                 }
