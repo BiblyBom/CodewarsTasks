31075d44152d57ffa1b965af0e044ebcd0dd883a
Maxim Boyev <maximboev90@gmail.com>
2022-07-28 21:54:21 +0300

    Добавлен CountingDuplicates.java с основной логикой

---
+++ src/main/java/Tasks/CountingDuplicates.java
@@ -0,0 +1,24 @@
+package Tasks;
+
+import java.util.HashSet;
+
+public class CountingDuplicates {
+
+    public static void main(String[] args) {
+        System.out.println(duplicateCount("aA11"));
+    }
+
+    public static int duplicateCount(String text) {
+        HashSet<Character> duplicates = new HashSet<>();
+        char[] characters = text.toLowerCase().toCharArray();
+        for (int i = 0; i < characters.length; i++) {
+            for (int j = i + 1; j < characters.length - 1; j++) {
+                if (characters[i] == characters[j]) {
+                    duplicates.add(characters[i]);
+                }
+            }
+        }
+
+        return duplicates.size();
+    }
+}
