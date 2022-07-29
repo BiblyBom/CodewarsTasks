26426f95a72b14962edb922a0a29e584f0f3236a
Maxim Boyev <maximboev90@gmail.com>
2022-07-29 23:00:17 +0300

    Исправлена ошибка со вторым символом в WhereIsMyParent.java. Добавлено условие с нулевой длиной строки

--- src/main/java/Tasks/WhereIsMyParent.java
+++ src/main/java/Tasks/WhereIsMyParent.java
@@ -8,10 +8,14 @@ public class WhereIsMyParent {
     }
 
     static String findChildren(final String text) {
+        if (text.length() == 0) {
+            return "";
+        }
+
         char[] characters = text.toLowerCase().toCharArray();
         Arrays.sort(characters);
         characters[0] -= 32;
-        for (int i = 1; i < characters.length - 1; i++) {
+        for (int i = 0; i < characters.length - 1; i++) {
             if (characters[i] != characters[i + 1] && characters[i] + 32 != characters[i + 1]) {
                 characters[i + 1] -= 32;
             }
