3f3f5099559d54db591c79ba5bd85cd793b72f60
Maxim Boyev <maximboev90@gmail.com>
2022-07-29 22:53:43 +0300

    Добавлен WhereIsMyParent.java с основной логикой

---
+++ src/main/java/Tasks/WhereIsMyParent.java
@@ -0,0 +1,22 @@
+package Tasks;
+
+import java.util.Arrays;
+
+public class WhereIsMyParent {
+    public static void main(String[] args) {
+        System.out.println(findChildren("AbbaaBbcCc"));
+    }
+
+    static String findChildren(final String text) {
+        char[] characters = text.toLowerCase().toCharArray();
+        Arrays.sort(characters);
+        characters[0] -= 32;
+        for (int i = 1; i < characters.length - 1; i++) {
+            if (characters[i] != characters[i + 1] && characters[i] + 32 != characters[i + 1]) {
+                characters[i + 1] -= 32;
+            }
+        }
+        return new String(characters);
+    }
+
+}
