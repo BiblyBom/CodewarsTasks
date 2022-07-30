52e7e0d9054e1be7c8fd8e934e33e95dcf59f5bc
Maxim Boyev <maximboev90@gmail.com>
2022-07-31 00:59:51 +0300

    В MessageValidator.java добавлена основная логика

--- src/main/java/Tasks/MessageValidator.java
+++ src/main/java/Tasks/MessageValidator.java
@@ -1,11 +1,28 @@
 package Tasks;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MessageValidator {
     public static void main(String[] args) {
-
+        System.out.println(isAValidMessage("4code13hellocodewars"));
     }
 
     public static boolean isAValidMessage(String message) {
-        return false;
+        Pattern pattern = Pattern.compile("[a-z]+|\\d+");
+        Matcher matcher = pattern.matcher(message);
+        ArrayList<String> matchesList = new ArrayList<>();
+        while (matcher.find()) {
+            matchesList.add(matcher.group());
+        }
+        for (int i = 0; i < matchesList.size() - 1; i += 2) {
+            if (Integer.parseInt(matchesList.get(i)) != matchesList.get(i + 1).length()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
