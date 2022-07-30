016ed990cc3b455786370af70bfd7f4ad658d9c8
Maxim Boyev <maximboev90@gmail.com>
2022-07-31 01:54:48 +0300

    В MessageValidator.java исправлены ошибки с регистром букв, пустой строкой, нечетным количеством элементов в строке, обработано исключение NumberFormatException

--- src/main/java/Tasks/MessageValidator.java
+++ src/main/java/Tasks/MessageValidator.java
@@ -1,24 +1,39 @@
 package Tasks;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MessageValidator {
     public static void main(String[] args) {
-        System.out.println(isAValidMessage("4code13hellocodewars"));
+        System.out.println(isAValidMessage("5UOiTr"));
     }
 
     public static boolean isAValidMessage(String message) {
-        Pattern pattern = Pattern.compile("[a-z]+|\\d+");
+        if (message.equals("") || message.equals("0")) {
+            return true;
+        }
+        if (message.matches("\\s+")) {
+            return true;
+        }
+        Pattern pattern = Pattern.compile("[a-z,A-Z]+|\\d+");
         Matcher matcher = pattern.matcher(message);
         ArrayList<String> matchesList = new ArrayList<>();
         while (matcher.find()) {
             matchesList.add(matcher.group());
         }
+        if (matchesList.size() % 2 != 0) {
+            return false;
+        }
+
         for (int i = 0; i < matchesList.size() - 1; i += 2) {
-            if (Integer.parseInt(matchesList.get(i)) != matchesList.get(i + 1).length()) {
+            int nextInt = 0;
+            try {
+                nextInt = Integer.parseInt(matchesList.get(i));
+            } catch (NumberFormatException e) {
+                return false;
+            }
+            if (nextInt != matchesList.get(i + 1).length()) {
                 return false;
             }
         }
