a4c2a8042b7ccd1def501cefd0f87861e112d6ea
Maxim Boyev <maximboev90@gmail.com>
2022-07-30 23:44:58 +0300

    Добавлен MessageValidator.java

---
+++ src/main/java/Tasks/MessageValidator.java
@@ -0,0 +1,11 @@
+package Tasks;
+
+public class MessageValidator {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isAValidMessage(String message) {
+        return false;
+    }
+}
